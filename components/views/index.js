4b9d22fbba5a6d6c482b7320a54701a76e3f3566
lilbigangel86 <lilbigangel86@yahoo.com>
2023-02-20 11:52:27 -0500

    Jan 20 update

---
+++ components/views/index.js
@@ -0,0 +1,5 @@
+export { default as About } from "./About";
+export { default as contact } from "./contact";
+export { default as group } from "./group";
+export { default as Home } from "./Home";
+export { default as Individual } from "./Individual";
--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-let happy = "First line of code";
-console.log(happy);
+import { Footer, Header, Main, Nav } from "./components";
+import * as store from "./store";
+import Navigo from "navigo";
+import { capitalize } from "lodash";
+const router = new Navigo("/");
+
+function render(state = store.Home) {
+  document.querySeletor("#root").innerHTML = `
+  ${Header(state)}
+  ${Nav(store.Links)}
+  ${Main(state)}
+  ${Footer()}
+  `;
+  router.updatePageLinks();
+}
+
+router
+  .on({
+    "/": () => render(),
+    ":view": params => {
+      let view = capitalize(params.data.view);
+      render(store[view]);
+    }
+  })
+  .resolve();
