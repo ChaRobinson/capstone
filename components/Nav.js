ac0b4d14a2fd77e87fc426d2f0ce6cad897c84b6
lilbigangel86 <lilbigangel86@yahoo.com>
2023-02-26 04:54:27 -0500

    26 Feb Update 1

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,7 @@ import html from "html-literal";
 
 export default links => html`
   <nav>
-    <ul class="nav-bar" id="navigation">
+    <ul class="nav-bar">
       ${links.map(
         el =>
           `<li><a href="/${el.title}" title="${el.title}" data-navigo>${el.text}</a></li>`
--- components/views/Home.js
+++ components/views/Home.js
@@ -1,18 +1,35 @@
 import html from "html-literal";
 export default () => html`
-  <header></header>
+  <!-- <header></header> -->
   <h1>
-    My First Heading
+    Walk With Us
   </h1>
   <div class="introduction">
     <p class="introduction">
-      still changing? yes
+      Are you a mother or a mother to be who may be suffering from a Perinatal
+      Mood Disorder?
+      <br />
+      <br />
+      <!-- Insert Picture Here -->
+      Come and let us help you start your journey to heal through the power of
+      walking.
     </p>
+    <p></p>
   </div>
   <div class="map">
     <!-- Insert MAP API HERE -->
     <p class="map">
       Find out how many mothers have signed up by inputting your zip code!
     </p>
+
+    <section class="introduction">
+      <p>
+        If you or someone you know may be struggling with suicidal thoughts, you
+        can call the U.S. National Suicide Prevention Lifeline by dialing 988,
+        or calling 800-273-TALK (8255) any time day or night, or chat online.
+        Crisis Text Line also provides free, 24/7, confidential support via text
+        message to people in crisis when they dial 741741
+      </p>
+    </section>
   </div>
 `;
