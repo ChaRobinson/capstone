d89ae462e4e95fd6ecd6856bd1fe5ae7f6543111
lilbigangel86 <lilbigangel86@yahoo.com>
2023-03-04 17:01:02 -0500

    4MAR 2023

--- components/views/contact.js
+++ components/views/contact.js
@@ -1,8 +1,10 @@
 import html from "html-literal";
 
 export default () => html`
-  <p>
-    Want to contact me? E-mail me at cmrobinson8686@gmail.com I look forward to
-    hearing from you!
-  </p>
+  <section class="something">
+    <p>
+      Want to contact me? E-mail me at cmrobinson8686@gmail.com I look forward
+      to hearing from you!
+    </p>
+  </section>
 `;
