d08fbb0b16713adfc4d186064a2a39f240b7af3f
Charity R <110882544+ChaRobinson@users.noreply.github.com>
2023-03-04 16:57:36 -0500

    Delete contact.js

--- components/views/contact.js
+++
@@ -1,8 +0,0 @@
-import html from "html-literal";
-
-export default () => html`
-  <p>
-    Want to contact me? E-mail me at cmrobinson8686@gmail.com I look forward to
-    hearing from you!
-  </p>
-`;
